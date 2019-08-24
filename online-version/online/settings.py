afff8f44c73844010757e33658998109eb332582
Gavin Foutz <gavinafoutz@gmail.com>
2019-08-24 02:55:05 -0400

    Added homepage, some auth groundwork

--- online-version/online/settings.py
+++ online-version/online/settings.py
@@ -31,13 +31,22 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    # my own
+    'main',
+    'register',
+
 ]
+
+CRISPY_TEMPLATE_PACK = 'uni_form'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- online-version/online/views.py
+++ online-version/online/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 def index(request):
     output = "Amazon Price Scraper"
     return HttpResponse(output)
+
+def register(response):
+    output = ""
+    return HttpResponse(output)

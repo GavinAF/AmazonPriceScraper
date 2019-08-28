bdb4941baa7509448ad01439fd1f81b1c31d1421
Gavin Foutz <gavinafoutz@gmail.com>
2019-08-27 22:55:58 -0400

    Updated forms on homepage

--- online-version/main/forms.py
+++ online-version/main/forms.py
@@ -2,10 +2,19 @@ from django import forms
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from crispy_forms.helper import FormHelper
 
 class RegisterForm(UserCreationForm):
+    helper = FormHelper()
     email = forms.EmailField()
+    
+    def __init__(self, *args, **kwargs):
+        super(RegisterForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'email', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
 
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+        
--- online-version/main/views.py
+++ online-version/main/views.py
@@ -1,15 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import AuthenticationForm
 from .forms import RegisterForm
 
 # Create your views here.
 
 def home(response):
-    if response.method == "POST":
-        form = RegisterForm(response.POST)
-        if form.is_valid():
-            form.save()
-    else:
-        form = RegisterForm()
+    if response.method == 'POST':
+        if 'register' in response.POST:
+            register_form = RegisterForm(response.POST, prefix='register')
+            if register_form.is_valid():
+                register_form.save()
+            login_form = AuthenticationForm(prefix='login')
 
-    return render(response, "main/home.html", {"form":form})
+        elif 'login' in response.POST:
+            login_form = AuthenticationForm(data=response.POST, prefix='login')
+            if login_form.is_valid():
+                login_form.save() 
+                # login the user
+
+                return redirect("dashboard/")
+            register_form = RegisterForm(prefix='register')
+
+    else:
+        register_form = RegisterForm(prefix='register')
+        login_form = AuthenticationForm(prefix='login')
+    
+    return render(response, "main/home.html", {"login_form":login_form, "register_form":register_form})
+
+
+#def home(response):
+ #   if response.method == "POST":
+  #      form = RegisterForm(response.POST)
+   #     if form.is_valid():
+    #        form.save()
+    #else:
+     #   form = RegisterForm()
+
+    #return render(response, "main/home.html", {"form":form})

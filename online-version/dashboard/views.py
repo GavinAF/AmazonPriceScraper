e602c35288f941cc888a1fbf861b112583b9655e
Gavin Foutz <gavinafoutz@gmail.com>
2019-08-30 13:28:04 -0400

    Created dashboard

--- online-version/dashboard/views.py
+++ online-version/dashboard/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+# Create views here.
+
+@login_required(login_url="/")
+def dashboard(response):
+
+    if response.user.is_authenticated:
+        username = response.user.username
+
+    return render(response, "dashboard/dashboard.html", {"username":username})

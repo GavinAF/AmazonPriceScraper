86593bba9853ef4df369977ea9095dc610eebbe7
Gavin Foutz <gavinafoutz@gmail.com>
2019-09-20 16:27:34 -0400

    Added CRON_CLASSES

--- online-version/online/settings.py
+++ online-version/online/settings.py
@@ -125,6 +125,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+CRON_CLASSES = [
+    "fire_scraper.check_prices_job",
+]
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
